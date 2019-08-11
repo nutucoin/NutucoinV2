f66067631e1ac1936092b0240f982e439ad71410
tran <nguyenhoangtran11@yahoo.com>
2019-08-11 14:22:22 +0700

    Update Copyright notation for NutuCoin
    Signed-off-by: tran <nguyenhoangtran11@yahoo.com>

--- src/univalue/lib/univalue.cpp
+++ src/univalue/lib/univalue.cpp
@@ -1,5 +1,6 @@
 // Copyright 2014 BitPay Inc.
 // Copyright 2015 Bitcoin Core Developers
+// Copyright (c) 2019 The NutuCoin developers 
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
