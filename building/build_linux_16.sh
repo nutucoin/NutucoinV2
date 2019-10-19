4cebacb0c757aa282eee81ac1fc10d5e764eea5b
tran <nguyenhoangtran11@yahoo.com>
2019-10-20 00:32:27 +0700

    Add the script to build on Ubuntu 16
    Signed-off-by: tran <nguyenhoangtran11@yahoo.com>

---
+++ building/build_linux_16.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+export LC_ALL=C
+
+CUR_DIR=$(pwd)
+
+SCRIPT_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+SRC_PATH=$( cd $SCRIPT_PATH/.. && pwd)
+echo $SRC_PATH
+
+# Install all dependencies to build nutucoin
+$SCRIPT_PATH/install-dependencies.sh
+
+cd $SRC_PATH || exit
+./autogen.sh
+./configure --disable-tests CFLAGS=-fPIC
+make
+
+rm -rf release
+mkdir -p release
+cp src/nutucoind release/
+cp src/nutucoin-cli release/
+cp src/qt/nutucoin-qt release/
+strip release/*
+
+cd $CUR_DIR || exit
