9e32121b8536506efff88a677e81b358fbee5628
tran <nguyenhoangtran11@yahoo.com>
2020-01-21 01:14:22 +0700

    Update copyright year
    Signed-off-by: tran <nguyenhoangtran11@yahoo.com>

--- src/algo/hash_algos.h
+++ src/algo/hash_algos.h
@@ -1,6 +1,6 @@
 
 // Copyright (c) 2017-2019 The Raven Core developers
-// Copyright (c) 2019 The NutuCoin developers 
+// Copyright (c) 2019-2020 The NutuCoin developers 
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
